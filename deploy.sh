c2c703f61ef7bf7eae34ad59c3e41e1c110d7c31
Ying Huang <yhuang@imvu.com>
2018-01-04 23:23:42 -0800

    n

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-copyDir="~/myproject/git/AI/setup"
+copyDir="/media/usb/docker-mldonkey"
 while read ss
 do
     #ssh -n $ss "apt-get update && apt-get install realpath"
