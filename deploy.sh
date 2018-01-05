50483f4a43792c6767dd6262dc9c8dd33722bc28
Ying Huang <yhuang@imvu.com>
2018-01-04 23:18:07 -0800

    n

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+copyDir="~/myproject/git/AI/setup"
+while read ss
+do
+    #ssh -n $ss "apt-get update && apt-get install realpath"
+    ./copy.sh "$ss" "$copyDir"
+    ssh -n $ss "$copyDir/run.sh"
+done < deploy.list
